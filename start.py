4978d67835e14b468db25251e0fbb8cd9de94d24
GRAYH3ART <65398165+GRAYH3ART@users.noreply.github.com>
2020-05-15 14:08:08 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,17 @@
+import platform
+
+
+
+print ('PythonTestGitProject')
+
+
+
+print(platform.system())
+
+print(platform.system())
+
+print(platform.machine())
+
+print(platform.version())
+
+print(platform.processor())
